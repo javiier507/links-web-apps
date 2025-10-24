6d7daac0d394152f248b2d186b5e07d1ab087500
penalbaxplor <carlos@getxplor.com>
2025-10-23 23:04:19 -0500

    feat(extension): implementing react google oauth

--- apps/extension/src/main.tsx
+++ apps/extension/src/main.tsx
@@ -1,13 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { helloWorld } from '@repo/api/index'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./index.css";
+import App from "./App.tsx";
+import { helloWorld } from "@repo/api/index";
 
-helloWorld()
+helloWorld();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+if (!CLIENT_ID) {
+    throw new Error("VITE_GOOGLE_CLIENT_ID is not defined in environment variables");
+}
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <GoogleOAuthProvider clientId={CLIENT_ID}>
+            <App />
+        </GoogleOAuthProvider>
+    </StrictMode>,
+);
