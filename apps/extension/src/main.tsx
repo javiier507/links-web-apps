28ff4714241c310160131f9892991d0ed9246d60
penalbaxplor <carlos@getxplor.com>
2025-11-25 23:44:35 -0500

    chore: reverting disabling api in extension

--- apps/extension/src/main.tsx
+++ apps/extension/src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-/* import { helloWorld } from '@repo/api/index'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { helloWorld } from "@repo/api/index";
+import App from "./App.tsx";
 
-helloWorld() */
+helloWorld();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
