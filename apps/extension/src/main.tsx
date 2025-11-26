a3660710c3278cf6befa2414adf591a7749d09c3
penalbaxplor <carlos@getxplor.com>
2025-11-25 23:32:25 -0500

    disabling api in extension

--- apps/extension/src/main.tsx
+++ apps/extension/src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { helloWorld } from '@repo/api/index'
+/* import { helloWorld } from '@repo/api/index'
 
-helloWorld()
+helloWorld() */
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
