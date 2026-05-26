f08f881b6475a563923d2299d828745f9fd1ffec
penalbaxplor <carlos@getxplor.com>
2026-05-25 23:49:06 -0500

    refactor(web): restoring host fetch metadata variable

--- apps/web/libs/api/resources.ts
+++ apps/web/libs/api/resources.ts
@@ -48,8 +48,8 @@ export async function DeleteLink(linkId: string) {
 
 async function FetchMetadata(url: string): Promise<Metadata> {
     try {
-        const baseUrl = process.env.VERCEL_URL
-            ? `https://${process.env.VERCEL_URL}`
+        const baseUrl = process.env.NEXT_PUBLIC_APP_URL
+            ? `https://${process.env.NEXT_PUBLIC_APP_URL}`
             : "http://localhost:3000";
         const response = await fetch(`${baseUrl}/api/metadata`, {
             method: "POST",
